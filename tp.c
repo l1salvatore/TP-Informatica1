87294f561dc945130ac8d22790c56a05102a47a6
ngravic <nicograbich@gmail.com>
2016-06-08 15:03:53 -0300

    injdak

--- tp.c
+++ tp.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 #define SECTOR 0
 #define CANTIDAD 1
 
@@ -9,9 +8,10 @@ void filtrar(char TipoSensor) {
     float CotaInf, CotaSup, Medicion;
     FILE *mediciones = fopen("mediciones.dat","r");
     fscanf(mediciones,"%d %c %f %f %f %d %d",&ID,&TipoSensor_,&CotaInf,&CotaSup,&Medicion,&Hora,&Minuto);
+    printf("\t\tID_Sensor\tMedicion\n");
     while(feof(mediciones)==0){
             if (TipoSensor_ == TipoSensor) 
-                printf("%d\t%c\t%f\t%f\t%f\t%d\t%d\n",ID,TipoSensor_,CotaInf,CotaSup,Medicion,Hora,Minuto);
+                printf("\t\t%d\t\t%f\n",ID,Medicion);
             fscanf(mediciones,"%d %c %f %f %f %d %d",&ID,&TipoSensor_,&CotaInf,&CotaSup,&Medicion,&Hora,&Minuto);
     }
     fclose(mediciones);
@@ -60,17 +60,17 @@ void contarAlarmas(int (*alarmas)[6]){
 }
 
 void ordenarAlarmas(int (*alarmas)[6]){
-   int i,aux,aux1,swap = 1;
+   int i,aux,swap = 1;
    while(swap == 1){
 	swap = 0;
 	for(i=0;i<5;i++)
 	  if (alarmas[CANTIDAD][i] < alarmas[CANTIDAD][i+1]){
 		aux = alarmas[SECTOR][i];
-		aux1 = alarmas[CANTIDAD][i];
 		alarmas[SECTOR][i] = alarmas[SECTOR][i+1];
-		alarmas[CANTIDAD][i] = alarmas[CANTIDAD][i+1];
 		alarmas[SECTOR][i+1] = aux;
-		alarmas[CANTIDAD][i+1] = aux1;
+		aux = alarmas[CANTIDAD][i];
+		alarmas[CANTIDAD][i] = alarmas[CANTIDAD][i+1];
+		alarmas[CANTIDAD][i+1] = aux;
 		swap = 1;
 	  }	
   }
@@ -80,24 +80,26 @@ void AlarmasXSector() {
    int j,alarmas[2][6];
    contarAlarmas(alarmas);
    ordenarAlarmas(alarmas);
-   printf("Alarmas por sector\n");
+   printf("\t\tAlarmas por sector\n");
    for(j=0;j<6;j++)
-	printf("Sector %d: %d alarmas/s\n",alarmas[SECTOR][j],alarmas[CANTIDAD][j]);
+	printf("\t\tSector %d: %d alarmas/s\n",alarmas[SECTOR][j],alarmas[CANTIDAD][j]);
 }
 
 
 
 void validarOp(int *i, int min, int max){
     while (*i < min || *i > max){
-        printf("Opc invalida, vuelva a ingresar!\n");        
+        printf("AVISO:Opcion invalida, vuelva a ingresar!\n"); 
+        printf("Ingrese Opcion>");       
         scanf(" %i",&*i);
     }
 }
 
 void validarVariableTipo(char *c){
     while ((*c != 'T') && (*c != 'P') && (*c != 'N') && (*c != 'C')) {
-        printf("Variable invalida, vuelva a ingresar!\n");
-        fflush(stdin);        
+        printf("AVISO:Variable invalida, vuelva a ingresar!\n");
+        fflush(stdin);
+        printf("Ingrese la variable>");        
         scanf(" %c",&*c);
     }
 }
@@ -110,12 +112,14 @@ void mostrarMenu(){
     printf("2- Generar alarmas\n");
     printf("3- Alarmas por sector\n");
     printf("4- Salir\n");
+    printf("Ingrese Opcion>");
 }
 
 
 
 main() {
     int op=-5;
+    int ArchAlarmasGenerado = 0;
     char c;
     while (op != 4) {
         mostrarMenu();
@@ -123,17 +127,21 @@ main() {
         validarOp(&op,1,4);
         switch (op) {
             case 1:
-                printf("Ingrese la variable\n");
+                printf("Ingrese la variable>");
                 fflush(stdin);
                 scanf(" %c",&c);
                 validarVariableTipo(&c);
                 filtrar(c);
                 break;
             case 2:
-		generarAlarmas();
+		        generarAlarmas();
+		        ArchAlarmasGenerado=1;
                 break;
             case 3:
-		AlarmasXSector();
+		        if (ArchAlarmasGenerado==0)
+		            printf("AVISO:Primero tiene que generar las alarmas!\n");
+		        else 
+		            AlarmasXSector();
                 break;
         }
     }
